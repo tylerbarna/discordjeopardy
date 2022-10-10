d1855aed6d05bb60bd7e9715f32d5ee7fa30b402
Tyler Barna <tylerpbarna@gmail.com>
2022-10-10 16:31:46 -0500

    made secrets.json
    github workflow was kinda opaque, so I opted for just a secrets.json file

--- bot.py
+++ bot.py
@@ -13,7 +13,11 @@ import time
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('BOT_TOKEN')
+## read secrets.json and get token
+json_file = open("secrets.json")
+TOKEN = json.load(json_file)["token"]
+
+#TOKEN = os.environ.get('BOT_TOKEN')
 
 client = discord.Client(intents=discord.Intents.default())
 
